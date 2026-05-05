c612aaf4bc1743edda6e5a1215e797338f5e65dd
Milan Hauth <milahu@milahu.duckdns.org>
2026-05-05 22:24:08 +0200

    fix: SuryaModel LOAD REPORT: Status: MISSING

--- surya/common/surya/__init__.py
+++ surya/common/surya/__init__.py
@@ -104,10 +104,14 @@ class SuryaModel(S3DownloaderMixin, SuryaPreTrainedModel):
     _supports_static_cache = True
     _supports_attention_backend = True
     main_input_name = "input_ids"
-    _tied_weights_keys = ["lm_head.weight"]
+    _tied_weights_keys = [
+        # fix: SuryaModel LOAD REPORT: Status: MISSING
+        # "lm_head.weight"
+    ]
     # fix: AttributeError: 'SuryaModel' object has no attribute 'all_tied_weights_keys'
     all_tied_weights_keys = {
-        "lm_head.weight": "model.embed_tokens.weight",
+        # fix: SuryaModel LOAD REPORT: Status: MISSING
+        # "lm_head.weight": "model.embed_tokens.weight",
         # "lm_head.weight": "embed_tokens.weight",
     }
 
